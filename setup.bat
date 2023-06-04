3a90f5b985053ba2c94d3d9f0d523e9672688835
TechGuru#1101 <121064329+anyiskiidd@users.noreply.github.com>
2023-06-04 19:31:13 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -8,9 +8,14 @@ pip install -r requirements.txt
 if %errorlevel% equ 0 (
     echo.
     echo Installation completed successfully.
+    echo.
+    echo Running main.py...
+    echo.
+
+    python main.py
 ) else (
     echo.
-    echo An error occurred during installation.
+    echo An error occurred during installation. The program will not be run.
 )
 
 echo.
